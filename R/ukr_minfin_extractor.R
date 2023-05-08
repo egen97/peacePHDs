618c29838c27a3108b50810c51d743a50a057829
Eric G. E. Nilsen <e.g.e.nilsen@stv.uio.no>
2023-05-08 19:27:15 +0200

    wrong pipe

--- R/ukr_minfin_extractor.R
+++ R/ukr_minfin_extractor.R
@@ -12,7 +12,7 @@
 ukr_minfin_extractor <- function(xml_document, css_nr_id = NULL , data_pattern = "'.*'"){
   if(is.null(css_nr_id)) stop("You need to provide the id nr for each graph-point")
   if(length( css_nr_id) > 1) warning("In css_nr_id =. The condition has length > 1 and only the first element will be used")
-  x <- xml_document %>%
+  x <- xml_document |>
     rvest::html_element(paste0("svg > rect:nth-child", paste0("(", css_nr_id, ")"))) |>
     as.character() |>
     stringr::str_extract(data_pattern) |>
